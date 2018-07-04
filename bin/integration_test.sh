f183658675feace650fc584a15529b691480f2ab
erulabs <seandon.mooy@gmail.com>
2018-07-03 17:50:45 -0700

    skip startup on CI

--- bin/integration_test.sh
+++ bin/integration_test.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 source ./bin/_variables.sh
 
-if [[ -z ${SKIP_STARTUP} ]]; then
-  . ./bin/_startup_local.sh
-else
-  . ./bin/_find_compose_services.sh
+if [[ -z ${CI} ]]; then
+  if [[ -z ${SKIP_STARTUP} ]]; then
+    . ./bin/_startup_local.sh
+  else
+    . ./bin/_find_compose_services.sh
+  fi
 fi
 
-if [[ "$1" == "--docker" || ! -z ${CI} ]]; then
+if [[ "$1" == "--docker" ]]; then
   echo "-> Via docker"
   docker-compose -p "${PROJECT}" run test
 else
